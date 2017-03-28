83961a949b6ae321de744e16227c023d9c7068ad
alkorion <al.lira.co@gmail.com>
2017-03-28 15:55:52 -0700

    Comment final print loop. Add debug print statement

--- tcpl/chapter1/src/ex_1-13.c
+++ tcpl/chapter1/src/ex_1-13.c
@@ -20,26 +20,42 @@ main() {
     for (int i = 0; i < 10; ++i) {
         ndigitsw[i] = 0;
     }
-
+    
     state = OUT;
     while ((c = getchar()) != EOF) {
+        /* check if end of word char */
         if (c==' ' || c=='\n' || c=='\t') {
             if (state == IN) {
+                /* if word ended, state=out and store word length */
                 state = OUT;
-                if (wl >= '0' && wl  <= '8')
+                if (wl >= '0' && wl  <= '9')
                     ++ndigitsw[wl-'1'];
-                else if (wl > '8') 
-                    ++ndigitsw['9'];
+                /* words longer than 10 char are stored in final bin */
+                else if (wl > '10') 
+                    ++ndigitsw['10'];
             }
         }
+        /* increment word-length counter while iterating through word */
         else {
             if (state == IN)
                 ++wl;
+            /* else start a new word-length count */
             else {
                 state = IN;
                 ++wl;
             }
         }
     }
-
+    printf("digits = ");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d", ndigitsw[1]);
+    }
+    printf("\n");
+    /*
+    for (int i=0; i<9; ++i) {
+        printf("%d\t", (i+1));
+        for (int j=0; j < ndigitsw[i]; ++j)
+            printf("+");
+        printf('\n');
+    } */
 }
