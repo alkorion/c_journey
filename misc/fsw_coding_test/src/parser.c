1a250a6665c48bb5faa8382bc3fd7f1f74ab6e27
Alessandro Lira <alira@terrabella.com>
2017-10-02 19:02:17 -0700

    Create 'blank' parser source file with some initial formatting

---
+++ misc/fsw_coding_test/src/parser.c
@@ -0,0 +1,9 @@
+/* FSW Coding Test
+ * Serial Stream Parser
+ * Alessandro Lira
+ *
+ * This program parses a serial stream into packets and displays the packet 
+ * payloads as a series of hexadecimal values
+ */
+
+#include<stdio.h>
